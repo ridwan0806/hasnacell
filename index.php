167633d317cd0cc96186b59111077097308df2e6
ridwan0806 <ridwanm32@gmail.com>
2022-03-07 22:06:30 +0700

    "menambahkan file index"

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Halaman Index</title>
+</head>
+<body>
+<h1>Halaman Index</h1>
+<a href="viewproduk.php">Data Produk</a><br></br>
+<a href="viewtrx.php">Data Transaksi</a><br></br>
+</body>
+</html>
