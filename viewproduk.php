fbdfd65ce3e671a9e2a32f50c3eaee3a98f9bf40
ridwan0806 <ridwanm32@gmail.com>
2022-03-07 22:27:45 +0700

    menambahan view

---
+++ viewproduk.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Halaman Produk</title>
+</head>
+<body>
+<h1>Daftar Produk</h1>
+	<table border="1" cellspacing="2" cellpadding="2">
+		<tr>
+			<th>No</th>
+			<th>ID</th>
+			<th>Nama Produk</th>
+			<th>Kategori</th>
+			<th>Brand</th>
+			<th>Harga Pokok</th>
+			<th>Harga Jual</th>
+			<th>Stok</th>
+		</tr>
+	</table>
+</body>
+</html>
