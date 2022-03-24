11be726633e82c2625b32e790da41fa2bfa710ce
ridwan0806 <ridwanm32@gmail.com>
2022-03-24 17:05:06 +0700

    menambahkan file detail transaksi

--- detailTrx.php
+++ detailTrx.php
@@ -2,6 +2,12 @@
 require 'function.php';
 
 $tgl = $_GET['tgl'];
+$result = lihat_transaksi_harian("SELECT a.id_produk AS ID, a.nama_produk AS NAMA, 
+                                b.qty AS QTY, a.harga_jual AS JUAL, a.hpp AS BELI, 
+                                SUM(a.harga_jual - a.hpp) AS LABA, SUM(b.qty * a.harga_jual) AS TOTAL 
+                                FROM produk a JOIN transaksi b ON a.`id_produk`=b.`id_produk` 
+                                WHERE b.tanggal='$tgl' GROUP BY a.id_produk 
+                                ");
 
 
 if (isset( $_POST['cari_produk']) ) {
@@ -37,6 +43,36 @@ if (isset( $_POST['cari_produk']) ) {
             </tr>
         </Table>
     </form>
+    <hr>
+
+    <table border="2">
+        <tr>
+            <td>No</td>
+            <td>Id Produk</td>
+            <td>Nama Produk</td>
+            <td>Qty Beli</td>
+            <td>Harga Jual</td>
+            <td>Harga Beli</td>
+            <td>Laba</td>
+            <td>Total Transaksi</td>
+        </tr>
+            <?php $no = 1; ?>
+            <?php foreach ($result as $row) { ?>
+        <tr>
+                <td><?= $no; ?></td>
+                <td><?= $row["ID"]; ?></td>
+                <td><?= $row["NAMA"]; ?></td>
+                <td><?= $row["QTY"]; ?></td>
+                <td><?= $row["JUAL"]; ?></td>
+                <td><?= $row["BELI"]; ?></td>
+                <td><?= $row["LABA"]; ?></td>
+                <td><?= $row["TOTAL"]; ?></td>
+        </tr>
+        <?php $no++; ?>
+        <?php }; ?>
+    </table>
+
+
 </body>
 </html>
 
--- function.php
+++ function.php
@@ -95,6 +95,17 @@ function tambahDataTransaksi($data) {
 	return mysqli_affected_rows($conn);
 }
 
+function lihat_transaksi_harian($query){
+	global $conn;
+
+	$result = mysqli_query($conn,$query);
+	$rows = [];
+	while ($row = mysqli_fetch_assoc($result) ) {
+		$rows[] = $row;
+	}
+	return $rows;
+}
+
 
 
 
